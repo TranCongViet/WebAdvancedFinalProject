4ae2a4b9ec49f8b964f36612884ca1bb76293ed5
TranCongViet <trancongviet990ntt@gmail.com>
2025-01-08 15:30:35 +0700

    Cấu hình react với vite và tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-400">Hello, Vite + React + TailwindCSS!</h1>
+    </div>
+  );
+}
+
+export default App
